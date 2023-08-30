e92e2d5650167befc7af0512bb7c6b308d84c19e
Nicholas Gannon <60980258+nickgannon@users.noreply.github.com>
2023-08-30 16:28:13 -0400

    Takes URL through input

--- WebScraper.py
+++ WebScraper.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-# URL of the web page you want to scrape
-url = "https://nicholasgannon.io/"
+# Prompt the user for the URL to be scraped
+url = input("Enter the URL to be scraped: ")
 
 # Send an HTTP GET request to the URL
 response = requests.get(url)
@@ -11,7 +11,7 @@ response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
 
 # Find all the article titles on the page
-article_titles = soup.find_all("h1")
+article_titles = soup.find_all("div")
 
 # Print the titles of the articles
 for title in article_titles:
