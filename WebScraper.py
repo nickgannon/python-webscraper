67261772c111e2184d493263bb78eb82e41094fc
Nicholas Gannon <60980258+nickgannon@users.noreply.github.com>
2023-09-04 10:03:01 -0400

    Update WebScraper.py
    Searches for h1, h2, h3, and div.
    Within output, there are labeled sections for each.
    Error message provided if none found.

--- WebScraper.py
+++ WebScraper.py
@@ -1,18 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Prompt the user for the URL to be scraped
-url = input("Enter the URL to be scraped: ")
+try:
+    # Prompt the user for the URL to be scraped
+    url = input("Enter the URL to be scraped: ")
 
-# Send an HTTP GET request to the URL
-response = requests.get(url)
+    # Send an HTTP GET request to the URL with a user-agent header
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
+    response = requests.get(url, headers=headers)
 
-# Parse the HTML content of the page using BeautifulSoup
-soup = BeautifulSoup(response.content, "html.parser")
+    # Check if the request was successful
+    response.raise_for_status()
 
-# Find all the article titles on the page
-article_titles = soup.find_all("div")
+    # Parse the HTML content of the page using BeautifulSoup
+    soup = BeautifulSoup(response.content, "html.parser")
 
-# Print the titles of the articles
-for title in article_titles:
-    print(title.text)
+    # Find all the article titles on the page (h1, h2, h3, and div)
+    h1_titles = soup.find_all("h1")
+    h2_titles = soup.find_all("h2")
+    h3_titles = soup.find_all("h3")
+    div_elements = soup.find_all("div")
+
+    # Print the titles of the articles
+    print("ALL H1:")
+    for title in h1_titles:
+        print(title.text)
+
+    print("\nALL H2:")
+    for title in h2_titles:
+        print(title.text)
+
+    print("\nALL H3:")
+    for title in h3_titles:
+        print(title.text)
+
+    print("\nALL DIV:")
+    for element in div_elements:
+        print(element.text)
+
+    if not h1_titles and not h2_titles and not h3_titles and not div_elements:
+        print("No <h1>, <h2>, <h3>, or <div> elements found on the page.")
+
+except requests.exceptions.RequestException as e:
+    print("Error: Unable to connect to the URL or invalid URL.")
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
